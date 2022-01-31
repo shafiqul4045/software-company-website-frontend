39df055ee9b3403b8812c637db14e84708cd9a45
mganahian <galib802863@gmail.com>
2022-01-31 20:00:56 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,17 @@
     // Client Testimonial Slider
 
     var swiper = new Swiper(".client-testimonial", {
+        loop: true,
+        speed: 1000,
+        autoplay:true,
+        delay:5000,
         navigation: {
             nextEl: ".ct-swiper-button-next",
             prevEl: ".ct-swiper-button-prev",
         },
     });
+    $('.video-frame').magnificPopup({
+        type: 'iframe'
+      });
 
 }(jQuery));
