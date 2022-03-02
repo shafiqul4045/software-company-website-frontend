f68a4d0598590530f1d6dccf60b07d78f64d0b3c
mganahian <galib802863@gmail.com>
2022-03-02 15:25:24 +0600

    scroll issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     // UPDATE: I was able to get this working again... Enjoy!
+      //  // Client Testimonial Slider
+      //  var swiper = new Swiper(".client-testimonial", {
+      //   navigation: {
+      //     nextEl: ".ct-swiper-button-next",
+      //     prevEl: ".ct-swiper-button-prev",
+      //   },
+      // });
 
 var cursor = document.querySelector('.cursor');
 var cursorinner = document.querySelector('.cursor2');
@@ -173,64 +180,12 @@ $('.search-cross-btn').on("click", function(){
        })
    }
 
-// Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml3');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml3 .letter',
-//     opacity: [0,1],
-//     easing: "easeInOutQuad",
-//     duration: 2250,
-//     delay: (el, i) => 150 * (i+1)
-//   }).add({
-//     targets: '.ml3',
-//     opacity: 0,
-//     duration: 300,
-//     easing: "easeOutExpo",
-//     delay: 100
-//   });
+    $('.video-frame').magnificPopup({
+        type: 'iframe'
+      });
 
-//     const cursor = document.querySelector('.cursor');
-
-//     document.addEventListener('mousemove', e => {
-//         cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-//     })
-
-//     document.addEventListener('click', () => {
-//         cursor.classList.add("expand");
-
-//         setTimeout(() => {
-//             cursor.classList.remove("expand");
-//         }, 500)
-//     })
-    
-   
-    // var categoryWrap = document.querySelectorAll('.collapse-menu-slide');
-    // var categoryToggleIcon = document.querySelectorAll('.collapse-menu span');
-    // var collapseMenu = document.querySelector('.collapse-menu');
-
-    // categoryToggleIcon.forEach((element)=>{
-    //     element.addEventListener('click', ()=>{
-    //     categoryWrap.forEach((e)=>{
-    //         e.classList.toggle('collapse-menu-active')
-    //     })
-    //     })
-    // })
-    
-    // var categoryWrap = document.querySelectorAll('.collapse-menu-slide');
-    // var categoryToggleIcon = document.querySelectorAll('.collapse-menu');
-    // categoryToggleIcon.forEach((element)=>{
-    //     element.addEventListener('click', ()=>{
-    //     categoryWrap.forEach((e)=>{
-    //         e.classList.toggle('collapse-menu-unactive')
-    //     })
-    //     })
-    // })
-
-    // Client Testimonial Slider
-
+ 
     // var swiper = new Swiper(".client-testimonial", {
     //     loop: true,
     //     speed: 1000,
@@ -241,10 +196,5 @@ $('.search-cross-btn').on("click", function(){
     //         prevEl: ".ct-swiper-button-prev",
     //     },
     // });
-    $('.video-frame').magnificPopup({
-        type: 'iframe'
-      });
-
-
       
 }(jQuery));
